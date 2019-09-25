799c59fcdc4e4bf874686643071119ca9cfca7a6
Gman107 <gmannan62@gmail.com>
2019-09-25 18:24:44 -0400

    test

--- flaskblog.py
+++ flaskblog.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from forms import RegistrationForm, LoginForm
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
